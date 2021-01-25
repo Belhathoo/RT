cc3a2d615375434522dfdd538248c848bfd9fe96
belhathoo <belrow@Bader-LapTop.localdomain>
2021-01-25 14:04:11 +0100

    1.4.3

--- libxml/xml_check.c
+++ libxml/xml_check.c
@@ -19,8 +19,6 @@ int    check_tag(char  *t_name)
     */
 }
 
-int     check_attr(char *t_name, char *a_name)
-{
 /*
     attr depends on tag's name
     attributes for each Object depend on the value of the Name's atrr_Value
@@ -30,24 +28,29 @@ int     check_attr(char *t_name, char *a_name)
 /*
     only one of the 3 (col || txt || noise)
 */
+
+int     check_attr(char *t_name, char *a_name)
+{
+
 // printf("---%s---\n", t_name);
 // printf("\t-%s-\n", a_name);
-
     if (!ft_strcmp(t_name, "Object"))
-        if (!ft_strcmp(a_name, "name") || !ft_strcmp(a_name, "position")\
+       { if (!ft_strcmp(a_name, "name") || !ft_strcmp(a_name, "position")\
             || !ft_strcmp(a_name, "direction") ||!ft_strcmp(a_name, "translation")\
             || !ft_strcmp(a_name, "rotation") || !ft_strcmp(a_name, "radius")\
-            || !ft_strcmp(a_name, "color") || !ft_strcmp(a_name, "material"))
-            return (1);
-    if (!ft_strcmp(t_name, "Camera"))
-        if (!ft_strcmp(a_name, "lookfrom") || !ft_strcmp(a_name, "lookat")\
+            || !ft_strcmp(a_name, "color") || !ft_strcmp(a_name, "texture")
+            || !ft_strcmp(a_name, "noise") || !ft_strcmp(a_name, "material"))
+            return (1);}
+    else if (!ft_strcmp(t_name, "Camera"))
+       { if (!ft_strcmp(a_name, "lookfrom") || !ft_strcmp(a_name, "lookat")\
                 || !ft_strcmp(a_name, "fov"))
-            return (1);
-    if (!ft_strcmp(t_name, "Option"))
-        if (!ft_strcmp(a_name, "anti-aliasing") || !ft_strcmp(a_name, "filtre"))
-            return (1);
-    if (!ft_strcmp(t_name, "Light"))
-        if (!ft_strcmp(a_name, "type") || !ft_strcmp(a_name, "position"))
-            return (1);
+            return (1);}
+    else if (!ft_strcmp(t_name, "Option"))
+        {if (!ft_strcmp(a_name, "anti-aliasing") || !ft_strcmp(a_name, "filtre"))
+            return (1);}
+    else if (!ft_strcmp(t_name, "Light"))
+        {if (!ft_strcmp(a_name, "type") || !ft_strcmp(a_name, "position")\
+        || !ft_strcmp(a_name, "intensity") || !ft_strcmp(a_name, "color"))
+            return (1);}
     return (0);
 }
